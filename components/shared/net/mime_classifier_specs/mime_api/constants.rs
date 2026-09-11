c491454fa2a11e53a549ea335b5f502d190c4ef6
wood-ghost <yizhiy@kth.se>
2026-09-11 16:31:38 +0200

    remove essence macro lemma

--- components/shared/net/mime_classifier_specs/mime_api/constants.rs
+++ components/shared/net/mime_classifier_specs/mime_api/constants.rs
@@ -17,6 +17,7 @@ verus! {
 
 // https://docs.rs/mime/0.3.17/src/mime/lib.rs.html#750
 // TEXT_PLAIN, "text/plain", 4;
+#[verifier::auto_reveal_literals(strlit)]
 pub open spec fn text_plain_identity() -> MimeView {
     MimeView {
         type_: "text"@,
@@ -28,6 +29,7 @@ pub open spec fn text_plain_identity() -> MimeView {
 
 // https://docs.rs/mime/0.3.17/src/mime/lib.rs.html#751
 // TEXT_PLAIN_UTF_8, "text/plain; charset=utf-8", 4, None, 10;
+#[verifier::auto_reveal_literals(strlit)]
 pub open spec fn text_plain_utf_8_identity() -> MimeView {
     MimeView {
         type_: "text"@,
@@ -122,6 +124,7 @@ pub open spec fn image_bmp_identity() -> MimeView {
 
 // https://docs.rs/mime/0.3.17/src/mime/lib.rs.html#779
 // APPLICATION_OCTET_STREAM, "application/octet-stream", 11;
+#[verifier::auto_reveal_literals(strlit)]
 pub open spec fn application_octet_stream_identity() -> MimeView {
     MimeView {
         type_: "application"@,
--- components/shared/net/mime_classifier_specs/mime_api/essence_lemmas.rs
+++ components/shared/net/mime_classifier_specs/mime_api/essence_lemmas.rs
@@ -3,52 +3,29 @@ use vstd::prelude::*;
 
 use super::views::*;
 
-macro_rules! define_mime_essence_lemmas {
-    (
-        $group_name:ident {
-            $(
-                $lemma_name:ident => ($type_:literal, $subtype:literal)
-            ),* $(,)?
-        }
-    ) => {
-        verus! {
-            $(
-                #[verifier::auto_reveal_literals(strlit)]
-                pub(crate) broadcast proof fn $lemma_name(mt: &Mime)
-                    requires
-                        view(mt).type_ =~= ($type_)@,
-                        view(mt).subtype =~= ($subtype)@,
-                        // view(mt).suffix is None, // for servo behavior
-                    ensures
-                        #[trigger] essence_str(mt) =~= (concat!($type_, "/", $subtype))@,
-                {}
-            )*
-
-            pub(crate) broadcast group $group_name {
-                $(
-                    $lemma_name,
-                )*
-            }
-        }
-    };
-}
-
 verus! {
 
-define_mime_essence_lemmas! {
-    mime_essence_str_lemmas {
-        lemma_image_bmp_essence_str => ("image", "bmp"),
-        lemma_image_png_essence_str => ("image", "png"),
-        lemma_image_gif_essence_str => ("image", "gif"),
-        lemma_image_jpeg_essence_str => ("image", "jpeg"),
+pub(crate) broadcast proof fn lemma_essence_str(mt: &Mime, expected: &str)
+    requires
+        // view(mt).suffix is None, // for servo behavior
+        expected@.len() == view(mt).type_.len() + 1 + view(mt).subtype.len(),
+        forall|i: int| 0 <= i < expected@.len() ==> #[trigger] expected@[i] ==
+            if i < view(mt).type_.len() {
+                view(mt).type_[i]
+            } else if i == view(mt).type_.len() {
+                '/'
+            } else {
+                view(mt).subtype[i - view(mt).type_.len() - 1]
+            },
+    ensures
+        #![trigger essence_str(mt), expected@]
+        essence_str(mt) =~= expected@,
+{
+    assert(essence_str(mt) =~= expected@);
+}
 
-        lemma_text_html_essence_str => ("text", "html"),
-        lemma_text_xml_essence_str => ("text", "xml"),
-        lemma_text_plain_essence_str => ("text", "plain"),
-
-        lemma_application_pdf_essence_str => ("application", "pdf"),
-        lemma_application_octet_stream_essence_str => ("application", "octet-stream"),
-    }
+pub(crate) broadcast group mime_essence_str_lemmas {
+    lemma_essence_str,
 }
 
 } // verus!
